57a0ddf02e59abae239348909885b56931066a41
Clara Jeon <johndoe@email.com>
2024-12-05 10:56:47 -0500

    fixed golomb comparisons

--- comparisons.py
+++ comparisons.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from golomb_encoding import compress_sequence as golomb_compress  # Golomb compression
 from elias_encoding import compress_sequence as elias_compress  # Elias compression
+from Bio import SeqIO
 
 def get_sequence_files(input_folder):
     """
@@ -25,10 +26,10 @@ def compress_sequence_and_track(sequence_file, reference_file, identifier, encod
 
     # Read the sequence from the file
     with open(sequence_file, "r") as f:
-        seq = f.read().strip()
+        seq = str(SeqIO.read(sequence_file, "fasta").seq)
 
     # Compress the sequence using the selected encoding type
-    reference_seq = open(reference_file, "r").read().strip()
+    reference_seq = str(SeqIO.read(reference_file, "fasta").seq)
 
     if encoding_type == "golomb":
         bitarray_data, _ = golomb_compress(seq, reference_seq, m)
